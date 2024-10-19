cc02ca2eac78b4382c3b8d1dd85da3301e425567
Devan Rivera <devanrivera98@gmail.com>
2024-10-19 00:10:59 +0000

    desktop view of header along with customizations to themes

--- devan-portfolio/src/app/theme.tsx
+++ devan-portfolio/src/app/theme.tsx
@@ -1,8 +1,20 @@
 import { extendTheme } from "@chakra-ui/react";
 
 export const theme = extendTheme({
+  //font customization is for Chakra components only will not effect individual elements
   fonts: {
-    heading: 'var(--font-playfair)',
-    body: 'var(--font-open-sans)'
-  }
-})
+    heading: 'var(--font-montserrat)',
+    body: 'var(--font-open-sans)',
+  },
+  styles: {
+  //globally state the desire fonts for when Chakra components arent used
+    global: {
+      'html, body': {
+        fontFamily: 'var(--font-open-sans)',
+      },
+      h1: {
+        fontFamily: 'var(--font-montserrat)',
+      },
+    },
+  },
+});
