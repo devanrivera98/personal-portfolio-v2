f305b0b3b033ebdb8d1bf34961818390352da1f6
Devan Rivera <devanrivera98@gmail.com>
2024-10-21 23:00:50 +0000

    charka dark and light mode implementations

--- devan-portfolio/src/app/providers.tsx
+++ devan-portfolio/src/app/providers.tsx
@@ -1,8 +1,27 @@
 'use client'
 import { theme } from './theme'
 
-import { ChakraProvider } from '@chakra-ui/react'
+import { ChakraProvider, ColorModeScript } from '@chakra-ui/react'
+
+/**
+ * Providers component wraps the application with necessary global providers.
+ * It ensures that Chakra UI's theme and color mode settings are available across the app.
+ *
+ * - ChakraProvider: Applies the custom theme to all components within the app.
+ * - ColorModeScript: Injects the correct color mode (light/dark) based on the user's preference
+ *   or system settings. This ensures the correct color mode is applied on the initial load,
+ *   avoiding a flash of incorrect mode.
+ */
 
 export function Providers({ children }: { children: React.ReactNode }) {
-  return <ChakraProvider theme={theme}>{children}</ChakraProvider>
+
+  return (
+    <>
+  <ColorModeScript initialColorMode={theme.initialColorMode} />
+  <ChakraProvider theme={theme}>
+    {/* The script lets the corrrect color mode be applied on inital load */}
+    {children}
+  </ChakraProvider>
+  </>
+  )
 }
