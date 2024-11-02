2cabf911274dcf849516e42042d349b3d2535e04
Devan Rivera <devanrivera98@gmail.com>
2024-11-02 00:19:52 +0000

    made responsive adjustments to main portfolio image

--- devan-portfolio/src/app/components/Introduction/Introduction.tsx
+++ devan-portfolio/src/app/components/Introduction/Introduction.tsx
@@ -6,9 +6,9 @@ export default function Introduction() {
 
   return (
     <>
-      <section className='mt-20 grid lg:grid-cols-2'>
-        <div className='flex flex-col gap-y-10'>
-          <h1 className='text-5xl lg:text-7xl'>Hello, I'm Devan Rivera.</h1>
+      <section className='mt-14 flex flex-col lg:flex-row'>
+        <div className='intro-text-container flex flex-col gap-y-10'>
+          <h1 className='text-5xl lg:text-7xl'>Hello, <br className='hidden lg:block'></br> I'm Devan Rivera.</h1>
           <h2 className='text-4xl text-darkRed'>I Transform Pixels Into Ideas</h2>
           <div className=''>
             <p className='text-2xl'>
@@ -22,9 +22,9 @@ export default function Introduction() {
             </div>
           </div>
         </div>
-        <div className='hidden lg:flex items-center justify-center'>
+        <div className='hidden intro-img-container lg:flex items-center justify-center'>
           <div className='portfolio-image-container'>
-          <Image className='portfolio-image' src='/images/portfolio-photo.jpg' alt='devan-photo' width={2000} height={2600} layout="responsive" />
+          <Image className='portfolio-image' src='/images/portfolio-photo.jpg' alt='devan-photo' width={1000} height={1300} layout="responsive" priority/>
           </div>
         </div>
       </section>
