908e4bea4257de50258a5ab782f6f2aa606a09aa
Devan Rivera <devanrivera98@gmail.com>
2024-11-09 23:35:08 +0000

    aos animations added to main page

--- devan-portfolio/src/app/layout.tsx
+++ devan-portfolio/src/app/layout.tsx
@@ -1,13 +1,23 @@
 "use client";
 import { Providers } from "./Providers";
+import { useEffect } from "react";
 import { fonts } from "./fonts";
 import "./globals.scss";
+import Aos from "aos";
+import "aos/dist/aos.css";
 
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  useEffect(() => {
+    Aos.init({
+      // duration: 500,
+      once: true,
+    });
+  }, []);
+
   return (
     <html
       lang="en"
