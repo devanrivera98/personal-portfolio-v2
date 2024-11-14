584bafc40e0c643ad914110bc1808320f5dbb011
Devan Rivera <devanrivera98@gmail.com>
2024-11-14 03:23:29 +0000

    outlined desktop view for each project card

--- devan-portfolio/src/app/components/Projects/ProjectCard.tsx
+++ devan-portfolio/src/app/components/Projects/ProjectCard.tsx
@@ -1,24 +1,41 @@
 import ProjectSkillsSection from "./ProjectSkillSection";
+import { FaLink } from "react-icons/fa6";
+import { FaGithub } from "react-icons/fa";
 
 export default function ProjectCard() {
   return (
     <>
-    <section className="py-28 grid lg:grid-cols-2">
-      <div className="flex flex-col pr-4">
-        <h1 className="text-2xl">Advanced Building Services</h1>
-        <p className="py-3">Advanced Building Services, is a leading HVAC company for which I developed an informative, user-friendly webpage. The site effectively showcases the company's services and integrates EmailJS to facilitate seamless communication between the business and its clients. I prioritized optimizing the user experience across all devices, ensuring that the website is fully responsive and intuitive on both mobile and desktop platforms.</p>
-        <ProjectSkillsSection />
-        <div className="mt-auto pr-2 flex justify-end gap-7">
-          <button className="py-2 px-4 bg-darkRed rounded">Live Demo</button>
-          <button className="py-2 px-4 bg-mediumGray rounded">GitHub</button>
+      <section className="py-28 grid lg:grid-cols-2 items-start lg:items-stretch">
+        <div className="order-2 lg:order-1 flex flex-col pr-4">
+          <h1 className="text-2xl">Advanced Building Services</h1>
+          <p className="py-3">
+            I developed a responsive, user-friendly website for Advanced Building Services, a leading HVAC company, to showcase their services and streamline client communication with EmailJS integration.
+          </p>
+          <ProjectSkillsSection />
+          <div className="buttons-container">
+            <button className="navigation-button bg-red-600 hover:bg-darkRed text-white">
+              <span className="pr-2">
+                <FaLink />
+              </span>
+              Live Demo
+            </button>
+            <button className="github-button">
+              <span className="pr-2">
+                <FaGithub />
+              </span>
+              GitHub
+            </button>
+          </div>
         </div>
-      </div>
-      <div>
-        <picture>
-          <img className="rounded-xl w-full h-full " src="./images/advanced.png"></img>
-        </picture>
-      </div>
-    </section>
+        <div className="order-1 lg:order-2 project-image-container">
+          <picture>
+            <img
+              className="rounded-xl w-full object-fit"
+              src="./images/advanced.png"
+            ></img>
+          </picture>
+        </div>
+      </section>
     </>
-  )
+  );
 }
--- devan-portfolio/src/app/components/Projects/ProjectSkillSection.tsx
+++ devan-portfolio/src/app/components/Projects/ProjectSkillSection.tsx
@@ -1,20 +1,25 @@
-const buildingServicesSkills = ['React', 'TypeScript', 'Tailwind CSS', 'CSS3', 'HTML5', 'EmailJS', 'Vite']
+const buildingServicesSkills = [
+  "React",
+  "TypeScript",
+  "Tailwind CSS",
+  "CSS3",
+  "HTML5",
+  "EmailJS",
+  "Vite",
+];
 
 export default function ProjectSkillsSection() {
-
-  const allSkills = buildingServicesSkills.map((skill) =>
-  <>
-    <div className="pr-4 my-3" >
-      <span className="text-lg bg-mediumGray px-2 py-1 shrink-0 rounded">{skill}</span>
-    </div>
-  </>
-  )
+  const allSkills = buildingServicesSkills.map((skill) => (
+    <>
+      <div className="pr-4 my-3">
+        <span className="skill-span">{skill}</span>
+      </div>
+    </>
+  ));
 
   return (
     <>
-    <div className="flex flex-wrap">
-      {allSkills}
-    </div>
+      <div className="flex flex-wrap">{allSkills}</div>
     </>
-  )
+  );
 }
