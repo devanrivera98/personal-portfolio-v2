b007c83f126e85e11c097f6edcfa103d3966e727
Devan Rivera <devanrivera98@gmail.com>
2024-11-23 01:11:31 +0000

    experience cards can now grow or shrink depending if the user wants to see more role description

--- devan-portfolio/src/app/components/About/Qualifications.tsx
+++ devan-portfolio/src/app/components/About/Qualifications.tsx
@@ -1,4 +1,4 @@
-import AllExperience from "./AllExperience"
+import AllExperience from "./AllExperience";
 
 export default function Qualifications() {
   return (
@@ -12,7 +12,9 @@ export default function Qualifications() {
           <p className="text-sm">Graduated in 2023</p>
         </div>
         <div>
-          <h2 className="text-xl font-medium">University of California, Irvine</h2>
+          <h2 className="text-xl font-medium">
+            University of California, Irvine
+          </h2>
           <p>Bachelor&#x2019;s Degree - Psychology</p>
           <p className="text-sm">Graduated in 2020</p>
         </div>
@@ -21,29 +23,7 @@ export default function Qualifications() {
       {/* Jobs Section */}
       <div className="flex flex-col">
         <h1 className="text-2xl font-bold pb-4">Experience</h1>
-        {/* <div className="mb-6">
-          <h2 className="text-xl font-bold">Front End Engineer</h2>
-          <p>Freelance</p>
-          <p className="text-sm">Jul 2023 - Present</p>
-          <div className="flex">
-            <p className="hidden overflow-hidden text-ellipsis whitespace-nowrap">Constructed a responsive user interface for Advanced Building Services’ landing page, improving user engagement and attracting 300+ unique visitors in the first month, with positive feedback on usability and functionality.</p>
-            <button className="hidden text-xs whitespace-nowrap">See More</button>
-            <ul className="flex flex-col gap-y-2 list-disc list-inside">
-              <li>Constructed a responsive user interface for Advanced Building Services’ landing page, improving user engagement and attracting 300+ unique visitors in the first month, with positive feedback on usability and functionality.</li>
-            </ul>
-          </div>
-        </div> */}
         <AllExperience />
-        <div className="mb-6">
-          <h2 className="text-xl font-bold">Billing Specialist</h2>
-          <p>Emergency Ambulance Service</p>
-          <p className="text-sm">Feb 2022 - Feb 2023</p>
-        </div>
-        <div>
-          <h2 className="text-xl font-bold">Retail Associate</h2>
-          <p>Adidas</p>
-          <p className="text-sm">Sep 2018 - Jan 2021</p>
-        </div>
       </div>
     </div>
   );
--- devan-portfolio/src/app/components/About/about-component/ExperienceCard.tsx
+++ devan-portfolio/src/app/components/About/about-component/ExperienceCard.tsx
@@ -1,23 +1,47 @@
-export default function ExperienceCard({title, company, date, firstDuty, duties }: {title: string, company: string, firstDuty: string, date: string, duties: string []}) {
+"use client";
 
-  const dutiesMap = duties.map((item, index) => (
-    <li key={index}>{item}</li>
-  ))
+import { useState } from "react";
+import SeeMoreButton from "../client/SeeMoreButton";
+import SeeLessButton from "../client/SeeLessButton";
+
+export default function ExperienceCard({
+  title,
+  company,
+  date,
+  firstDuty,
+  duties,
+}: {
+  title: string;
+  company: string;
+  firstDuty: string;
+  date: string;
+  duties: string[];
+}) {
+  const dutiesMap = duties.map((item, index) => <li key={index}>{item}</li>);
+
+  const [isOpen, setIsOpen] = useState(false);
 
   return (
     <>
-    <div className="mb-6">
-          <h2 className="text-xl font-bold">{title}</h2>
-          <p>{company}</p>
-          <p className="text-sm">{date}</p>
-          <div className="flex">
-            <p className="hidden overflow-hidden text-ellipsis whitespace-nowrap">{firstDuty}</p>
-            <button className="hidden text-xs whitespace-nowrap">See More</button>
-            <ul className="flex flex-col gap-y-2 list-disc list-inside">
-              {dutiesMap}
-            </ul>
-          </div>
+      <div className="mb-6">
+        <h2 className="text-xl font-bold">{title}</h2>
+        <p>{company}</p>
+        <p className="text-sm">{date}</p>
+        <div className={`${isOpen ? "hidden" : ""} flex`}>
+          <p className="overflow-hidden text-ellipsis whitespace-nowrap">
+            {firstDuty}
+          </p>
+          <SeeMoreButton isOpen={isOpen} setIsOpen={setIsOpen} />
         </div>
+        <div className={`${isOpen ? "" : "hidden"}`}>
+          <ul
+            className={`${isOpen ? "" : "hidden"} flex flex-col gap-y-2 list-disc list-inside`}
+          >
+            {dutiesMap}
+          </ul>
+          <SeeLessButton isOpen={isOpen} setIsOpen={setIsOpen} />
+        </div>
+      </div>
     </>
-  )
+  );
 }
