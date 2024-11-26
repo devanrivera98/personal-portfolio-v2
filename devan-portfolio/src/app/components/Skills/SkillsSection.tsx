67a0a932db6af31aefb9d7abd07a582f2ccc836e
Devan Rivera <devanrivera98@gmail.com>
2024-11-26 23:51:46 +0000

    added skill cards and dynamic colors for different modes

--- devan-portfolio/src/app/components/Skills/SkillsSection.tsx
+++ devan-portfolio/src/app/components/Skills/SkillsSection.tsx
@@ -1,13 +1,22 @@
+import SkillCard from "./SkillCard";
+import { allSkillsData } from "./data/allSkillsData";
+
 export default function SkillsSection() {
-
-
   return (
     <>
-    <section>
-      <div>
-        <h1 className="text-2xl font-bold">Skills</h1>
-      </div>
-    </section>
+      <section>
+        <div>
+          <h1 className="text-2xl font-bold">Skills</h1>
+        </div>
+        <div>
+          <SkillCard category="Languages" skills={allSkillsData.languages} />
+          <SkillCard
+            category="Frameworks &amp; Database"
+            skills={allSkillsData.frameworks}
+          />
+          <SkillCard category="Tools" skills={allSkillsData.tools} />
+        </div>
+      </section>
     </>
-  )
+  );
 }
