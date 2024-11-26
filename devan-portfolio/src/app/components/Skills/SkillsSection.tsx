72f049068e03ae978603340553acb2fafbabcee2
Devan Rivera <devanrivera98@gmail.com>
2024-11-26 00:14:49 +0000

    setup the skills section

---
+++ devan-portfolio/src/app/components/Skills/SkillsSection.tsx
@@ -0,0 +1,13 @@
+export default function SkillsSection() {
+
+
+  return (
+    <>
+    <section>
+      <div>
+        <h1 className="text-2xl font-bold">Skills</h1>
+      </div>
+    </section>
+    </>
+  )
+}
