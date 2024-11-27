dfc5302011c56d1899ff02bfa7eea7f6c2a4f780
Devan Rivera <devanrivera98@gmail.com>
2024-11-27 01:19:40 +0000

    added navigation functionally to easily navigate the website

--- devan-portfolio/src/app/components/Projects/ProjectSection.tsx
+++ devan-portfolio/src/app/components/Projects/ProjectSection.tsx
@@ -4,7 +4,7 @@ import "./projectSection.scss";
 export default function ProjectSection() {
   return (
     <>
-      <section className="pt-16 md:pt-32">
+      <section id="projects" className="pt-16 md:pt-32">
         <h1 className="project-headline"> FEATURED PROJECTS</h1>
         <AllProjects />
       </section>
