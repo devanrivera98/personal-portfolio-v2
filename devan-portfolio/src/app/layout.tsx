197457cc56777df2132a1f15665c9112519ffdf9
Devan Rivera <devanrivera98@gmail.com>
2025-07-26 16:42:26 -0700

    removed head in layout.tsx

--- devan-portfolio/src/app/layout.tsx
+++ devan-portfolio/src/app/layout.tsx
@@ -23,19 +23,6 @@ export default function RootLayout({
       suppressHydrationWarning
       className={`${fonts.montserrat.variable} ${fonts.openSans.variable} ${fonts.dancingScript.variable} `}
     >
-      <head>
-        {/* <!-- Google Tag Manager --> */}
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-      new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-      j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-      'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-      })(window,document,'script','dataLayer','GTM-NFC958D6');`,
-          }}
-        />
-        {/* <!-- End Google Tag Manager --> */}
-      </head>
       <body id="home">
         {/* <!-- Google Tag Manager (noscript) --> */}
         <noscript>
